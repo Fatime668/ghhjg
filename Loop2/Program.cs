1b1916f5b86b10738420da01ec5e2bd2ec566cb5
Fatime668 <99840451+Fatime668@users.noreply.github.com>
2022-03-16 12:45:12 +0400

    Add files via upload

---
+++ Loop2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Loop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int temp = n;
+            int num;
+            int sum = 0;
+            while (temp > 0)
+            {
+                num = temp % 10;
+                temp /= 10;
+                sum += num;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
